5ee92d2f4b9f2afc57e26897a620c18bfff894cc
seon-b <86736916+seon-b@users.noreply.github.com>
2024-07-01 09:08:42 -0400

    Upated automataGraphics.js

--- automataGraphics.js
+++ automataGraphics.js
@@ -2,20 +2,37 @@ const canvas = document.querySelector("#canvas");
 const canvasContext = canvas.getContext("2d");
 
 export class AutomataGraphics {
-  constructor(automataType) {
+  constructor(automataType = "DFA") {
     this.automataType = automataType;
     this.context = canvasContext;
   }
 
-  createState(
-    name = "undefined",
+  createState(name, stateType, xCoordinate, yCoordinate, radius) {
+    if (stateType === "final") this.context.lineWidth = 5;
+    if (stateType === "non-final") this.context.lineWidth = 1;
+    this.context.beginPath();
+    this.context.arc(xCoordinate, yCoordinate, radius, 0, Math.PI * 2, false);
+    this.context.stroke();
+  }
+
+  createNextTransition(
+    transitionValue,
     xCoordinate,
     yCoordinate,
-    symbolsArray = "undefined"
+    xCoordinate2,
+    yCoordinate2,
+    radius
   ) {
-    this.context.beginPath();
-    this.context.arc(xCoordinate, yCoordinate, 25, 0, Math.PI * 2, false);
-    this.context.lineTo(xCoordinate * 2, 50);
-    this.context.stroke();
+    if (xCoordinate > xCoordinate2) {
+      this.context.beginPath();
+      this.context.moveTo(xCoordinate - radius, yCoordinate);
+      this.context.lineTo(xCoordinate2 + radius, yCoordinate2);
+      this.context.stroke();
+    } else {
+      this.context.beginPath();
+      this.context.moveTo(xCoordinate + radius, yCoordinate);
+      this.context.lineTo(xCoordinate2 - radius, yCoordinate2);
+      this.context.stroke();
+    }
   }
 }
