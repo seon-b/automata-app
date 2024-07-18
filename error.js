93418425dceb6f9050bfe5309da738bbce6a9919
seon-b <86736916+seon-b@users.noreply.github.com>
2024-07-17 21:31:20 -0400

    Converted arrow functions to regular functions

--- error.js
+++ error.js
@@ -2,33 +2,33 @@ export let errorObject = {
   errorMessage: "",
 };
 
-export const setErrorObject = (message) => {
-  errorObject = { ...errorObject, errorMessage: message };
-};
-
-export const isInputEmpty = (input) => {
+export function isInputEmpty(input) {
   let regExPattern = /^\s*$/;
   if (regExPattern.test(input) === true) return regExPattern.test(input);
-};
+}
 
-export const isValidNumberOfStates = (numberOfStates, stateLimit) => {
+export function isValidNumberOfStates(numberOfStates, stateLimit) {
   if (numberOfStates < stateLimit) {
     return true;
   } else {
     return false;
   }
-};
+}
 
-export const isValidNumberOfTransitions = (
+export function isValidNumberOfTransitions(
   numberOfTransitionValues,
   stateLimit
-) => {
+) {
   if (numberOfTransitionValues > stateLimit - 1) {
     return false;
   } else {
     return true;
   }
-};
+}
+
+export function setErrorObject(message) {
+  errorObject = { ...errorObject, errorMessage: message };
+}
 
 export function validateInput(validationType, input) {
   if (validationType === "input") {
